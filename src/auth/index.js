9ec5347d6b525b50b6df4b4fe51f117d58768713
Shubham <saurabharyan30@gmail.com>
2020-03-11 21:42:54 +0530

    first commit

---
+++ src/auth/index.js
@@ -0,0 +1,62 @@
+export const signup = user => {
+  return fetch("https://college-backend.herokuapp.com/api/signup", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const signin = user => {
+  return fetch("https://college-backend.herokuapp.com/api/signin", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const authenticate = (jwt, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(jwt));
+    next();
+  }
+};
+
+export const signout = next => {
+  if (typeof window !== "undefined") localStorage.removeItem("jwt");
+  localStorage.removeItem("cart");
+  next();
+  return fetch("https://college-backend.herokuapp.com/api/signout", {
+    method: "GET"
+  })
+    .then(response => {
+      console.log("signout", response);
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const isAuthenticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
---
+++ src/user/apiUser.js
@@ -0,0 +1,66 @@
+export const read = (userId, token) => {
+  return fetch(`https://college-backend.herokuapp.com/api/user/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const update = (userId, token, user) => {
+  console.log("USER DATA UPDATE: ", user);
+  return fetch(`https://college-backend.herokuapp.com/api/user/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: user
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const remove = (userId, token) => {
+  return fetch(`https://college-backend.herokuapp.com/api/user/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const updateUser = (user, next) => {
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("jwt")) {
+      let auth = JSON.parse(localStorage.getItem("jwt"));
+      auth.user = user;
+      localStorage.setItem("jwt", JSON.stringify(auth));
+      next();
+    }
+  }
+};
+
+export const list = () => {
+  return fetch("https://college-backend.herokuapp.com/api/users", {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
